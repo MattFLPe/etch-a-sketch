7c883e8db2abfdaea2e06f24f90300c6d1cd478f
Matt <brasil.mateus748@gmail.com>
2023-08-04 15:50:34 -0300

    Added functionality to the buttons and added functions that changes color accordingly

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let color = "black";
+let click = true;
+
 function populateBoard(size){
 let board = document.querySelector('.board')
 let squares = board.querySelectorAll("div");
@@ -10,9 +13,7 @@ board.style.gridTemplateRows = `repeat(${size}, 1fr)`
 let amount = size * size;
 for(let i = 0; i < amount; i++){
    let square = document.createElement('div')
-   square.addEventListener("mouseover", () => { 
-    square.style.backgroundColor = "black";
-   });
+   square.addEventListener("mouseover", colorSquare);
     square.style.backgroundColor = 'white'
     board.insertAdjacentElement("beforeend", square)
 }
@@ -31,4 +32,34 @@ function changeSize(input){
         console.log("too many squares")
     }
 }
+// Enables random coloring of the squares
+function colorSquare(){
+if(click){
+    if(color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 300}, 100%, 50%)`
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+}
 
+function changeColor(choice){
+    color = choice
+}
+
+function resetBoard(){ 
+  let board = document.querySelector('.board')
+  let squares = document.querySelectorAll('div')
+  squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+document.querySelector('body').addEventListener('click', (e) => {
+    if(e.target.tagName != "BUTTON"){
+        click = !click;
+    if(click){
+        document.querySelector('.mode').textContent = "Mode: Coloring"
+    } else {
+        document.querySelector('.mode').textContent = "Mode: Not Coloring"
+    }
+    }
+});
